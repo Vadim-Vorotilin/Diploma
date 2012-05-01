a38693a1afe0b18453bcf068029b2b94bfb50927
Vadim Vorotilin <v.vorotilin@patrioticgames.org>
2012-05-01 13:56:22 +0300

    BeesColony class added.

---
+++ Diploma/BeesColony.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diploma
+{
+    public class BeesColony
+    {
+        public enum ProblemType
+        {
+            TSP,
+            VRP
+        }
+    }
+}
