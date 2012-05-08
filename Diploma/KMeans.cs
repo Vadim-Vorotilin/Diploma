0f6361232b24c123dbf4196338f9316db5ba0da0
Vadim Vorotilin <v.vorotilin@patrioticgames.org>
2012-05-08 19:32:41 +0300

    Cluster moved to separate file. K-Means added.

---
+++ Diploma/KMeans.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diploma
+{
+    public class KMeans
+    {
+        private List<Node> nodes;
+        private Cluster[] clusters;
+        private Node depot;
+
+        public KMeans(List<Node> nodes, int clustersCount)
+        {
+            this.nodes = new List<Node>();
+            this.nodes.AddRange(nodes);
+
+            clusters = new Cluster[clustersCount];
+
+            foreach (Node node in nodes)
+            {
+                if (node.Type == Node.NodeType.Depot)
+                {
+                    depot = node;
+                    break;
+                }
+            }
+
+            for (int i = 0; i != clusters.Length; i++)
+            {
+                clusters[i] = new Cluster();
+                clusters[i].Depot = depot;
+            }
+        }
+
+        private Node.Point[] centers;
+
+        private void SetCenters()
+        {
+            if (centers == null)
+            {
+                centers = new Node.Point[clusters.Length];
+
+                var xs = from node in nodes
+                         select node.RealPosition.x;
+
+                double xMin = xs.Min();
+                double xMax = xs.Max();
+
+                var ys = from node in nodes
+                         select node.RealPosition.y;
+
+                double yMin = ys.Min();
+                double yMax = ys.Max();
+
+                for (int i = 0; i != centers.Length; i++)
+                {
+                    double cx = TaskController.Rnd.NextDouble() * (xMax - xMin) + xMin;
+                    double cy = TaskController.Rnd.NextDouble() * (yMax - yMin) + yMin;
+
+                    centers[i] = new Node.Point(cx, cy);
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i != centers.Length; i++)
+                {
+                    centers[i] = clusters[i].Center;
+                }
+            }
+        }
+
+        private void GenerateClusters()
+        {
+            foreach (Cluster cluster in clusters)
+            {
+                cluster.Nodes.Clear();
+            }
+
+            foreach (Node node in nodes)
+            {
+                double minDist = double.PositiveInfinity;
+                Cluster closest = clusters[0];
+
+                for (int i = 0; i < centers.Length; i++)
+                {
+                    double dist = Node.Point.SqrDistance(centers[i], node.RealPosition);
+
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        closest = clusters[i];
+                    }
+                }
+
+                closest.Nodes.Add(node);
+            }
+        }
+
+        //private void 
+
+        public void Iteration()
+        {
+            SetCenters();
+            GenerateClusters();
+        }
+    }
+}
--- Diploma/SiteClustering.cs
+++ Diploma/SiteClustering.cs
@@ -9,62 +9,6 @@ namespace Diploma
     {
         public Cluster[] Clusters { get; private set; }
 
-        public class Cluster
-        {
-            public List<Node> Nodes = new List<Node>();
-            public Node Depot;
-
-            public Node.Point Center
-            {
-                get
-                {
-                    double sx = 0;
-                    double sy = 0;
-
-                    foreach (Node node in Nodes)
-                    {
-                        sx += node.RealPosition.x;
-                        sy += node.RealPosition.y;
-                    }
-
-                    return new Node.Point(sx / Nodes.Count, sy / Nodes.Count);
-                }
-            }
-
-            public double GetPrice ()
-            {
-                Node.Point center = Center;
-
-                double distance = 0;
-
-                foreach (Node node in Nodes)
-                {
-                    distance += Node.Point.SqrDistance(node.RealPosition, center);
-                }
-
-                if (Depot != null)
-                {
-                    distance += Node.Point.SqrDistance(Depot.RealPosition, center);
-                }
-
-                return distance;
-            }
-
-            public Cluster() {}
-
-            public Cluster(Cluster cluster)
-            {
-                Nodes = new List<Node>();
-
-                foreach (Node node in cluster.Nodes)
-                {
-                    Nodes.Add(node);
-                }
-
-                Depot = cluster.Depot;
-            }
-        }
-
         #region Overrides of Site
 
         public SiteClustering(List<Node> nodes, int clustersCount)
