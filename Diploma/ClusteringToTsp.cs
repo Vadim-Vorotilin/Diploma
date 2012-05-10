3763879a9b657b437b1ef6967a58ca4c5d1b2a48
Vadim Vorotilin <v.vorotilin@patrioticgames.org>
2012-05-11 00:51:58 +0300

    ClusteringToTsp class added.

---
+++ Diploma/ClusteringToTsp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diploma
+{
+    class ClusteringToTsp : Algorithm
+    {
+        public override double Value
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        protected override void InnerIteration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DrawNodes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
